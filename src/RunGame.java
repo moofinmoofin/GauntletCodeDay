d8c9eff911b1f8ffd4d7adf6b634cc1dea800f8f
moofinmoofin <buddycoppandas@gmail.com>
2015-02-14 17:43:50 -0500

    Cleaned up main.

--- src/RunGame.java
+++ src/RunGame.java
@@ -2,12 +2,10 @@ public class RunGame
 {
 	public static void main(String args[])
 	{
-	Window gameWin = new Window();
-	Map gameMap = new Map();
+	Frame gameWin = new Frame();
+	Tiles gameMap = new Tiles();
 	Char playerOne = new Char();
 	//Will communicate with movement and other char related classes. TODO
 	Enemy enemySet = new Enemy();
-	
-	System.out.println("balls to the walls");
 	}
 }
