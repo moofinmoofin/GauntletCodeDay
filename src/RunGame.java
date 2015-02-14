b37b66f7ebf0a83777f5d5ed91554688cccf893f
moofinmoofin <buddycoppandas@gmail.com>
2015-02-14 15:49:30 -0500

    chase's First commit!! yay!

---
+++ src/RunGame.java
@@ -0,0 +1,13 @@
+public class RunGame 
+{
+	public static void main(String args[])
+	{
+	Window gameWin = new Window();
+	Map gameMap = new Map();
+	Char playerOne = new Char();
+	//Will communicate with movement and other char related classes. TODO
+	Enemy enemySet = new Enemy();
+	
+	System.out.println("balls to the walls");
+	}
+}
